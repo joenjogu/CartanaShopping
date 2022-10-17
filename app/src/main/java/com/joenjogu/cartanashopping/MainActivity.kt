983238b4440fe5d1335be46109cd9a0b1780f8a2
joenjogu <joenjogu97@gmail.com>
2022-10-18 02:56:36 +0300

    feat: initial commit

---
+++ app/src/main/java/com/joenjogu/cartanashopping/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.joenjogu.cartanashopping
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.joenjogu.cartanashopping.ui.theme.CartanaShoppingTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CartanaShoppingTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    CartanaShoppingTheme {
+        Greeting("Android")
+    }
+}
