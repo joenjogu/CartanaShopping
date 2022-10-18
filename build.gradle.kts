d237b5a5aee87eee048d469e823a6e2de64b1395
joenjogu <joenjogu97@gmail.com>
2022-10-18 04:02:44 +0300

    chore: bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 buildscript {
     extra.apply {
-        set("compose_version", "1.1.1")
+        set("compose_version", "1.2.1")
     }
 }// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
