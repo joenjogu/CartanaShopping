809231fac2d4dcb4201e53ebbe0e934f41977ee0
joenjogu <joenjogu97@gmail.com>
2022-11-03 23:59:24 +0300

    feat: add daos

---
+++ core/database/src/main/java/com/joenjogu/cartanashopping/core/database/dao/CartDao.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2022 Joe Njogu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.joenjogu.cartanashopping.core.database.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.joenjogu.cartanashopping.core.database.entities.CartEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface CartDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertCartEntities(carts: List<CartEntity>)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertCartEntity(cart: CartEntity)
+
+    @Update
+    suspend fun updateCartEntity(cart: CartEntity)
+
+    @Query(
+        value = """
+        SELECT * FROM cart
+        WHERE id = :id
+    """
+    )
+    fun getCartEntityByID(id: String): Flow<CartEntity>
+}
